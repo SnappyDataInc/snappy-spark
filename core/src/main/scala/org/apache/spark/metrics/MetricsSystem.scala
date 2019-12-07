ed6f333ebc6150c368e6364429019e5ebdf725a3
Swati Mahajan <smahajan@snappydata.io>
2019-12-07 22:05:56 +0530

    Added few changes

--- core/src/main/scala/org/apache/spark/metrics/MetricsSystem.scala
+++ core/src/main/scala/org/apache/spark/metrics/MetricsSystem.scala
@@ -133,13 +133,12 @@ private[spark] class MetricsSystem private (
 
     if (instance == "driver" || instance == "executor") {
       if (metricsNamespace.isDefined && executorId.isDefined) {
-        // If sourceName contains either TIBCO ComputeDB or SnappyData then
-        // ignoring <app ID>.<executor ID (or "driver")> instead of
-        // that added unique clusterId along with sourceName
-        if (source.sourceName.contains("TIBCO ComputeDB")) {
+        if (source.sourceName.contains("TIBCO ComputeDB") ||
+            source.sourceName.contains("SnappyData")) {
+          // If sourceName contains either TIBCO ComputeDB or SnappyData then
+          // ignoring <app ID>.<executor ID (or "driver")> instead of
+          // that added unique clusterId along with sourceName
           MetricRegistry.name("", "", source.sourceName.replace(" ", "_"))
-        } else if (source.sourceName.contains("SnappyData")) {
-          MetricRegistry.name("", "", source.sourceName)
         } else {
           // for default spark metrics namespace
           MetricRegistry.name(metricsNamespace.get, executorId.get, source.sourceName)
