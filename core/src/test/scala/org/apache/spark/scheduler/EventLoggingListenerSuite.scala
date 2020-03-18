c4b402706ac45c0c359b498b5312b6ecf2f13faa
Sumedh Wale <swale@tibco.com>
2020-03-18 21:34:47 +0530

    [SNAPPYDATA] Add direct shipping of task data
    - Added a DirectBroadcast class for direct shipping of task data in the message when
      task data is small enough and/or number of partitions is small
    - Move streaming listener cleanup to SnappySession from SparkSession's finalize
    - Ensure cleanup in ThreadWithGarbageCleanup at the end of thread run instead of a finalizer
    - add checking of tests results for a few missing cases

--- core/src/test/scala/org/apache/spark/scheduler/EventLoggingListenerSuite.scala
+++ core/src/test/scala/org/apache/spark/scheduler/EventLoggingListenerSuite.scala
@@ -14,6 +14,24 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+/*
+ * Changes for TIBCO Project SnappyData data platform.
+ *
+ * Portions Copyright (c) 2017-2021 TIBCO Software Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you
+ * may not use this file except in compliance with the License. You
+ * may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License. See accompanying
+ * LICENSE file.
+ */
 
 package org.apache.spark.scheduler
 
@@ -209,7 +227,7 @@ class EventLoggingListenerSuite extends SparkFunSuite with LocalSparkContext wit
     sc.addSparkListener(eventExistenceListener)
 
     // Trigger asserts for whether the expected events are actually logged
-    sc.parallelize(1 to 10000).count()
+    sc.parallelize(1 to 10000).persist().count()
     sc.stop()
 
     // Ensure all asserts have actually been triggered
