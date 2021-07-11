crabo/spark

[SNAPPYDATA] Repl fix 3 (#200)
* Change class loader and serializer instance during task result getter if repl is
   in local properties. Unlike Spark where each repl is a spark app itself, in snappy multiple reple
   can be there at the same time and multiple non-repl sessions as well. So it is mandatory to
   set the appropriate serializer/de-serializer instance as per the exact repl instance in question.

core/src/main/scala/org/apache/spark/scheduler/TaskResultGetter.scala
@@ -54,6 +54,33 @@ private[spark] class TaskResultGetter(sparkEnv: SparkEnv, scheduler: TaskSchedul
     }
   }
 
+  private def getSerializer(taskSetManager: TaskSetManager): SerializerInstance = {
+    val props = taskSetManager.taskSet.properties
+    val classLoader: ClassLoader = if (props != null && !props.isEmpty) {
+      scheduler.getIntpClassLoader(props)
+    } else null
+
+    if (classLoader == null) return serializer.get
+    val ser = sparkEnv.closureSerializer
+    Thread.currentThread().setContextClassLoader(classLoader)
+    ser.setDefaultClassLoader(classLoader)
+    ser.newInstance()
+  }
+
+  private def getTaskResultSerializer(taskSetManager: TaskSetManager): SerializerInstance = {
+
+    val props = taskSetManager.taskSet.properties
+    val classLoader: ClassLoader = if (props != null && !props.isEmpty) {
+      scheduler.getIntpClassLoader(props)
+    } else null
+
+    if (classLoader == null) return taskResultSerializer.get
+    val ser = sparkEnv.serializer
+    Thread.currentThread().setContextClassLoader(classLoader)
+    ser.setDefaultClassLoader(classLoader)
+    ser.newInstance()
+  }
+
   def enqueueSuccessfulTask(
       taskSetManager: TaskSetManager,
       tid: Long,
@@ -61,7 +88,8 @@ private[spark] class TaskResultGetter(sparkEnv: SparkEnv, scheduler: TaskSchedul
     getTaskResultExecutor.execute(new Runnable {
       override def run(): Unit = Utils.logUncaughtExceptions {
         try {
-          val resultSerializer = taskResultSerializer.get()
+          // val resultSerializer = taskResultSerializer.get()
+          val resultSerializer = getTaskResultSerializer(taskSetManager)
           val (result, size) = resultSerializer.deserialize[TaskResult[_]](serializedData) match {
             case directResult: DirectTaskResult[_] =>
               if (!taskSetManager.canFetchMoreResults(serializedData.limit())) {
@@ -140,7 +168,9 @@ private[spark] class TaskResultGetter(sparkEnv: SparkEnv, scheduler: TaskSchedul
           val loader = Utils.getContextOrSparkClassLoader
           try {
             if (serializedData != null && serializedData.limit() > 0) {
-              reason = serializer.get().deserialize[TaskFailedReason](
+              // reason = serializer.get().deserialize[TaskFailedReason](
+              // serializedData, loader)
+              reason = getSerializer(taskSetManager).deserialize[TaskFailedReason](
                 serializedData, loader)
             }
           } catch {

core/src/main/scala/org/apache/spark/scheduler/TaskSetManager.scala
@@ -47,6 +47,7 @@ import org.apache.spark._
 import org.apache.spark.TaskState.TaskState
 import org.apache.spark.internal.{config, Logging}
 import org.apache.spark.scheduler.SchedulingMode._
+import org.apache.spark.serializer.SerializerInstance
 import org.apache.spark.util.{AccumulatorV2, Clock, LongAccumulator, SystemClock, Utils}
 import org.apache.spark.util.collection.MedianHeap
 
@@ -87,8 +88,8 @@ private[spark] class TaskSetManager(
   val speculationEnabled = conf.getBoolean("spark.speculation", false)
 
   // Serializer for closures and tasks.
-  val env = SparkEnv.get
-  val ser = env.closureSerializer.newInstance()
+  private[this] val env = SparkEnv.get
+  private[this] lazy val ser = env.closureSerializer.newInstance()
 
   val tasks = taskSet.tasks
   private[scheduler] val partitionToIndex = tasks.zipWithIndex
@@ -468,6 +469,19 @@ private[spark] class TaskSetManager(
       case (taskIndex, allowedLocality) => (taskIndex, allowedLocality, true)}
   }
 
+  private def getSerializer(task: Task[_]): SerializerInstance = {
+    val props = task.localProperties
+    val classLoader: ClassLoader = if (props != null && !props.isEmpty) {
+      sched.getIntpClassLoader(props)
+    } else null
+
+    if (classLoader == null) return ser
+    val ser1 = env.closureSerializer
+    Thread.currentThread().setContextClassLoader(classLoader)
+    ser1.setDefaultClassLoader(classLoader)
+    ser1.newInstance()
+  }
+
   /**
    * Respond to an offer of a single executor from the scheduler by finding a task
    *
@@ -536,7 +550,7 @@ private[spark] class TaskSetManager(
         }
         // Serialize and return the task
         val serializedTask: ByteBuffer = try {
-          ser.serialize(task)
+          getSerializer(task).serialize(task)
         } catch {
           // If the task cannot be serialized, then there's no point to re-attempt the task,
           // as it will always fail. So just abort the whole task-set.

SnappyDataInc/snappy-spark

core/src/main/scala/org/apache/spark/scheduler/TaskResultGetter.scala
@@ -54,6 +54,33 @@ private[spark] class TaskResultGetter(sparkEnv: SparkEnv, scheduler: TaskSchedul
     }
   }
 
+  private def getSerializer(taskSetManager: TaskSetManager): SerializerInstance = {
+    val props = taskSetManager.taskSet.properties
+    val classLoader: ClassLoader = if (props != null && !props.isEmpty) {
+      scheduler.getInterpreterClassLoader(props)
+    } else null
+
+    if (classLoader == null) return serializer.get
+    val ser = sparkEnv.closureSerializer
+    Thread.currentThread().setContextClassLoader(classLoader)
+    ser.setDefaultClassLoader(classLoader)
+    ser.newInstance()
+  }
+
+  private def getTaskResultSerializer(taskSetManager: TaskSetManager): SerializerInstance = {
+
+    val props = taskSetManager.taskSet.properties
+    val classLoader: ClassLoader = if (props != null && !props.isEmpty) {
+      scheduler.getInterpreterClassLoader(props)
+    } else null
+
+    if (classLoader == null) return taskResultSerializer.get
+    val ser = sparkEnv.serializer
+    Thread.currentThread().setContextClassLoader(classLoader)
+    ser.setDefaultClassLoader(classLoader)
+    ser.newInstance()
+  }
+
   def enqueueSuccessfulTask(
       taskSetManager: TaskSetManager,
       tid: Long,
@@ -61,7 +88,8 @@ private[spark] class TaskResultGetter(sparkEnv: SparkEnv, scheduler: TaskSchedul
     getTaskResultExecutor.execute(new Runnable {
       override def run(): Unit = Utils.logUncaughtExceptions {
         try {
-          val resultSerializer = taskResultSerializer.get()
+          // val resultSerializer = taskResultSerializer.get()
+          val resultSerializer = getTaskResultSerializer(taskSetManager)
           val (result, size) = resultSerializer.deserialize[TaskResult[_]](serializedData) match {
             case directResult: DirectTaskResult[_] =>
               if (!taskSetManager.canFetchMoreResults(serializedData.limit())) {
@@ -140,7 +168,9 @@ private[spark] class TaskResultGetter(sparkEnv: SparkEnv, scheduler: TaskSchedul
           val loader = Utils.getContextOrSparkClassLoader
           try {
             if (serializedData != null && serializedData.limit() > 0) {
-              reason = serializer.get().deserialize[TaskFailedReason](
+              // reason = serializer.get().deserialize[TaskFailedReason](
+              // serializedData, loader)
+              reason = getSerializer(taskSetManager).deserialize[TaskFailedReason](
                 serializedData, loader)
             }
           } catch {

core/src/main/scala/org/apache/spark/scheduler/TaskSetManager.scala
@@ -47,6 +47,7 @@ import org.apache.spark._
 import org.apache.spark.TaskState.TaskState
 import org.apache.spark.internal.{config, Logging}
 import org.apache.spark.scheduler.SchedulingMode._
+import org.apache.spark.serializer.SerializerInstance
 import org.apache.spark.util.{AccumulatorV2, Clock, LongAccumulator, SystemClock, Utils}
 import org.apache.spark.util.collection.MedianHeap
 
@@ -87,8 +88,8 @@ private[spark] class TaskSetManager(
   val speculationEnabled = conf.getBoolean("spark.speculation", false)
 
   // Serializer for closures and tasks.
-  val env = SparkEnv.get
-  val ser = env.closureSerializer.newInstance()
+  private[this] val env = SparkEnv.get
+  private[this] lazy val ser = env.closureSerializer.newInstance()
 
   val tasks = taskSet.tasks
   private[scheduler] val partitionToIndex = tasks.zipWithIndex
@@ -468,6 +469,19 @@ private[spark] class TaskSetManager(
       case (taskIndex, allowedLocality) => (taskIndex, allowedLocality, true)}
   }
 
+  private def getSerializer(task: Task[_]): SerializerInstance = {
+    val props = task.localProperties
+    val classLoader: ClassLoader = if (props != null && !props.isEmpty) {
+      sched.getInterpreterClassLoader(props)
+    } else null
+
+    if (classLoader == null) return ser
+    val ser1 = env.closureSerializer
+    Thread.currentThread().setContextClassLoader(classLoader)
+    ser1.setDefaultClassLoader(classLoader)
+    ser1.newInstance()
+  }
+
   /**
    * Respond to an offer of a single executor from the scheduler by finding a task
    *
@@ -536,7 +550,7 @@ private[spark] class TaskSetManager(
         }
         // Serialize and return the task
         val serializedTask: ByteBuffer = try {
-          ser.serialize(task)
+          getSerializer(task).serialize(task)
         } catch {
           // If the task cannot be serialized, then there's no point to re-attempt the task,
           // as it will always fail. So just abort the whole task-set.