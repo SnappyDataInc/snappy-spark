apache/spark

[SNAP-1233] clear InMemorySorter before calling its reset (#35)
This is done so that any spill call (due to no EVICTION_DOWN) from within the spill
call will return without doing anything, else it results in NPE trying to read
page tables which have already been cleared.

core/src/main/java/org/apache/spark/util/collection/unsafe/sort/UnsafeExternalSorter.java
@@ -225,7 +225,17 @@ public final class UnsafeExternalSorter extends MemoryConsumer {
     // Note that this is more-or-less going to be a multiple of the page size, so wasted space in
     // pages will currently be counted as memory spilled even though that space isn't actually
     // written to disk. This also counts the space needed to store the sorter's pointer array.
-    inMemSorter.reset();
+
+    // temporarily clear inMemorySorter so that a recursive spill call will return
+    final UnsafeInMemorySorter memSorter = inMemSorter;
+    if (memSorter != null) {
+      inMemSorter = null;
+      try {
+        memSorter.reset();
+      } finally {
+        inMemSorter = memSorter;
+      }
+    }
     // Reset the in-memory sorter's pointer array only after freeing up the memory pages holding the
     // records. Otherwise, if the task is over allocated memory, then without freeing the memory
     // pages, we might not be able to get memory for the pointer array.

SnappyDataInc/snappy-spark

core/src/main/java/org/apache/spark/util/collection/unsafe/sort/UnsafeExternalSorter.java
@@ -230,7 +230,17 @@ public final class UnsafeExternalSorter extends MemoryConsumer {
     // Note that this is more-or-less going to be a multiple of the page size, so wasted space in
     // pages will currently be counted as memory spilled even though that space isn't actually
     // written to disk. This also counts the space needed to store the sorter's pointer array.
-    inMemSorter.freeMemory();
+
+    // temporarily clear inMemorySorter so that a recursive spill call will return
+    final UnsafeInMemorySorter memSorter = inMemSorter;
+    if (memSorter != null) {
+      inMemSorter = null;
+      try {
+        memSorter.freeMemory();
+      } finally {
+        inMemSorter = memSorter;
+      }
+    }
     // Reset the in-memory sorter's pointer array only after freeing up the memory pages holding the
     // records. Otherwise, if the task is over allocated memory, then without freeing the memory
     // pages, we might not be able to get memory for the pointer array.